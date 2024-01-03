da2ee14a09fd8c55aaec650f8779def2d3c731a4
madddtone <madddtone@gmail.com>
2024-01-03 17:30:39 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,118 +1,165 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Table, Card, Button, Modal, Label, TextInput } from 'flowbite-react';
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [ data, setData] = useState([])
+  const [ dataModal, setDataModal ] = useState([])
+  const [ openModal, setOpenModal ] = useState(false)
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/get-post-data', {
+      method: 'GET',
+    });
+    const result = await response.json();
+    setData(result);
+  };
+
+  const generateData = async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/generate-post-data/2', {
+      method: 'POST',
+    });
+
+    fetchData()
+  };
+
+  const fetchPostByID = async (id) => {
+    const response = await fetch('http://127.0.0.1:8000/api/show-post-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({id: id}),
+    });
+
+    const data = await response.json()
+    setDataModal(data.Data)
+
+    setOpenModal(true)
+  };
+
+  const editDataModal = (e) => {
+    //copy dataModal ke newDataModal
+    const newDataModal = [...dataModal]
+    //edit variable baru
+    newDataModal[0] = { ...newDataModal[0], post_text: e };
+    //set kembali ke datamodal
+    setDataModal(newDataModal)
+
+    console.log('new data modal', newDataModal)
+  }
+
+  const handleUpdate = async () => {
+    const id = dataModal[0].id
+    const postText = dataModal[0].post_text
+
+    const response = await fetch('http://127.0.0.1:8000/api/update-post-data', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify({id: id, postText: postText})
+    })
+
+    setOpenModal(false)
+    fetchData()
+  }
+
+  // console.log('keadaan state data', dataModal)
+
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='flex justify-center'>
+
+     {/* this is card elements */}
+      <Card href="#" className="max-w-min mt-40">
+        <div className='flex justify-between'>
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Post Data
+          </h5>
+          <Button color="dark" onClick={generateData}>Generate Data</Button>
         </div>
-      </div>
+        <div className="overflow-x-auto">
+          
+          {/* this is table elements */}
+          <Table hoverable>
+            <Table.Head>
+              <Table.HeadCell>ID</Table.HeadCell>
+              <Table.HeadCell>Post</Table.HeadCell>
+              <Table.HeadCell>Created</Table.HeadCell>
+              <Table.HeadCell>Updated</Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Edit</span>
+              </Table.HeadCell>
+              <Table.HeadCell>
+                <span className="sr-only">Update</span>
+              </Table.HeadCell>
+            </Table.Head>
+            <Table.Body className="divide-y">
+              {data.map((index) => 
+              <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                  {index.id}
+                </Table.Cell>
+                <Table.Cell>
+                  {index.post_text}
+                </Table.Cell>
+                <Table.Cell>
+                  {index.created_at}
+                </Table.Cell>
+                <Table.Cell>
+                  {index.updated_at}
+                </Table.Cell>
+                <Table.Cell>
+                  <a href="#" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Edit
+                  </a>
+                </Table.Cell>
+                <Table.Cell>
+                  <a href="#" onClick={ ()=> fetchPostByID(index.id)} className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                    Update
+                  </a>
+                </Table.Cell> 
+              </Table.Row>
+              )}
+            </Table.Body>
+          </Table>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          {/* this is modal elements */}
+          <Modal show={openModal} onClose={() => setOpenModal(false)}>
+            {dataModal.map((data) =>
+              <>
+              <Modal.Header>Update {data.id}</Modal.Header>
+              <Modal.Body>
+                <div className="space-y-6">
+                  <div className="flex max-w-md flex-col gap-4">
+                    <div>
+                      <TextInput id="postText" type="text" addon="POST" placeholder="write your post" value={data.post_text} onChange={(e) => editDataModal(e.target.value)} required />
+                    </div>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button onClick={() => handleUpdate()}>
+                  Save
+                </Button>
+                <Button color="gray" onClick={() => setOpenModal(false)}>
+                  Cancel
+                </Button>
+              </Modal.Footer>
+              </>
+            )}
+          </Modal>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        </div>
+      </Card>
+    </div>
   )
 }
+
+
+
+
